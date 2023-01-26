ab14a7653122a2fa282737e48b62a826d6971e26
anas0 <ashraful0a@gmail.com>
2023-01-27 00:12:19 +0600

    Fundamental Concepts Array and Conditionals

---
+++ Milestone-4 Hello JavaScript/Module-18 Fundamental Concepts Array and Conditionals/arrray.js
@@ -0,0 +1,29 @@
+var ages = [23, 65, 95, 6, 11];
+console.log(ages);
+console.log(ages[2]);
+console.log(ages.length);
+
+ages[2] = 27;
+console.log(ages);
+
+var positionIndex = ages.indexOf(6);
+console.log(positionIndex);
+
+// push & pop 
+ages.push(56);
+console.log(ages);
+
+ages.pop();
+console.log(ages);
+
+ages.pop();
+console.log(ages);
+
+
+// condition 
+
+if(ages[3] >= 18){
+    console.log("You can get a NID");
+}else{
+    console.log('You are under age');
+}
