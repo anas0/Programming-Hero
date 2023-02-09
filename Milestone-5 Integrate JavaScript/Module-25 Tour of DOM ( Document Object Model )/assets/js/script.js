50deb92e7e038d8f328fdded964861c27fe3aade
anas0 <ashraful0a@gmail.com>
2023-02-09 19:25:45 +0600

    Tour of DOM

---
+++ Milestone-5 Integrate JavaScript/Module-25 Tour of DOM ( Document Object Model )/assets/js/script.js
@@ -0,0 +1,6 @@
+// querySelectorAll
+const querys = document.querySelectorAll('.container li');
+for(const query of querys){
+    query.style.border = "2px solid steelblue";
+    query.style.padding = "20px";
+}
